dc0d1b7881f91ff622c6c68692de4990b7d97981
Chitra Shrestha <ChitraShrestha12.com>
2025-11-16 14:35:33 +0545

    Sum of All Even Numbers up to N

--- Day-1_simple-number-problems/script.js
+++ Day-1_simple-number-problems/script.js
@@ -1,7 +1,7 @@
-const num = 5;
-let fact = 1;
+const num = 2000;
+let sumOfEven = 0;
 
 for (let i = 1; i <= num; i++) {
-  fact *= i;
+  if (i % 2 == 0) sumOfEven += i;
 }
-console.log(fact);
+console.log(sumOfEven);
