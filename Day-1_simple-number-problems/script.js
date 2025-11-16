bf1f118af33e842d2741c5e08610f4f3fac500b6
Chitra Shrestha <ChitraShrestha12.com>
2025-11-16 14:53:35 +0545

    Find the sum of all odd numbers up to N

--- Day-1_simple-number-problems/script.js
+++ Day-1_simple-number-problems/script.js
@@ -1,6 +1,8 @@
-const num = 30;
+const num = 10;
+let sumOfOdd = 0;
 for (let i = 1; i <= num; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log(i);
+  if (i % 2 != 0) {
+    sumOfOdd += i;
   }
 }
+console.log(sumOfOdd);
