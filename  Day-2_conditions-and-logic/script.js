401f72adec3a3221df86a00cfafd18473ac25091
Chitra Shrestha <ChitraShrestha12.com>
2025-11-17 11:16:52 +0545

    initialization Day-2_conditions-and-logic

---
+++  Day-2_conditions-and-logic/script.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 20;
+const c = 5;
+
+if (a > b && a > c) {
+  console.log("a is greater than b and c");
+} else if (b > a && b > c) {
+  console.log("b is greater than a and c");
+} else {
+  console.log("c is greater than a and b");
+}
