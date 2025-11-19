250c26e122840c71167b715658094db61da325cd
Chitra Shrestha <ChitraShrestha12.com>
2025-11-19 17:50:09 +0545

    Print Hollow Square Pattern

--- Day-3_Loops and Patterns/script.js
+++ Day-3_Loops and Patterns/script.js
@@ -1,12 +1,12 @@
-const rows = 5;
-for (let i = 1; i <= rows; i++) {
-  let str = "";
-  for (let j = 1; j<=rows-i+1; j++) {
-    str=str+" "
-  }
-  console.log(str);
-  for(let j=1;j<=2*i-1;j++){
-    str=str+"*"
+const rows = 4
+for(let i=1;i<=rows;i++){
+  let str = ''
+  for(let j=1;j<=rows;j++){
+    if(i==1 || i==rows || j==1 || j==rows){
+      str = str+'*'
+    }else{
+      str=str+' '
+    }
   }
   console.log(str);
 }
--- Day-3_Loops and Patterns/solutions.md
+++ Day-3_Loops and Patterns/solutions.md
@@ -54,3 +54,24 @@ for (let i = 1; i <= rows; i++) {
   console.log(str);
 }
 ```
+
+## Question 4
+
+Print Hollow Square Pattern.
+
+## Code
+
+```javascript
+const rows = 4
+for(let i=1;i<=rows;i++){
+  let str = ''
+  for(let j=1;j<=rows;j++){
+    if(i==1 || i==rows || j==1 || j==rows){
+      str = str+'*'
+    }else{
+      str=str+' '
+    }
+  }
+  console.log(str);
+}
+```
